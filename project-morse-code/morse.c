776a1e6fceb797bd77d70ef7afb1b339e2775e9c
hatul <przemek4651@o2.pl>
2020-12-13 13:38:48 +0100

    Started working on buffers

--- project-morse-code/morse.c
+++ project-morse-code/morse.c
@@ -6,20 +6,26 @@
 #include <asm/semaphore.h>
 #include <linux/module.h>
 #include <linux/ioctl.h>
-#include "console_struct.h"
+#include <linux/ctype.h>
+#include <linux/timer.h>
+
+#include <linux/delay.h>
+#include "/usr/src/linux/drivers/char/console_struct.h"
+
+#define currcons fg_console
 
 #define MAX_NUM_OF_DEVICES 8 
 #define BUFFER_START_SIZE 256
-#define DOT_START_TIME 50
-#define DASH_START_TIME 100
-#define PAUSE_START_TIME 200
+#define DOT_START_TIME 250
+#define DASH_START_TIME 500
+#define PAUSE_START_TIME 1000
 
-#define CHANGE_BUFF _IOW(60,1,int)
-#define CHANGE_DOT_TIME _IOW(60,2,int)
-#define CHANGE_DASH_TIME _IOW(60,3,int)
-#define CHANGE_PAUSE_TIME _IOW(60,4,int)
+#define CHANGE_BUFF _IOW(61,1,int)
+#define CHANGE_DOT_TIME _IOW(61,2,int)
+#define CHANGE_DASH_TIME _IOW(61,3,int)
+#define CHANGE_PAUSE_TIME _IOW(61,4,int)
 
-static const char *alpha[] = {
+static char *morse_chars[] = {
     ".-",   //A
     "-...", //B
     "-.-.", //C
@@ -47,7 +53,8 @@ static const char *alpha[] = {
     "-.--", //Y
     "--..", //Z
 };
-static const char *num[] = {
+
+static char *morse_digits[] = {
     "-----", //0
     ".----", //1
     "..---", //2
@@ -60,19 +67,37 @@ static const char *num[] = {
     "----.", //9
 };
 
-struct vc vc_cons [MAX_NR_CONSOLES];
+static char* morse_space[] = {" "}; //(space)
+
+
 
 struct body {
+    int* cons_nr;
+    int dot_time, dash_time, pause_time;
+    int usecount;
     int buffersize;
     char *buffer;
     int buffercount;
     int start, end;
-    int dot_time, dash_time, pause_time;
-    int usecount;
+    int transmitting;
     struct wait_queue *write_queue;
 };
+
+struct timer_list timer_reader;
+
 struct semaphore sem = MUTEX;
+struct semaphore sem_transmission = MUTEX;
 struct body transmitter[8];
+
+
+char* getMorseCode(char c){
+    if(c == ' ')
+        return morse_space[0];
+    if(isalpha(c))
+        return morse_chars[tolower(c) - 'a'];
+    if(isdigit(c))
+        return morse_digits[c - '0' + 26];
+}
 
 
 int morse_open(struct inode *inode, struct file *file) {
@@ -83,11 +108,7 @@ int morse_open(struct inode *inode, struct file *file) {
     down(&sem);
     MOD_INC_USE_COUNT;
     transmitter[number].usecount++;
-    if (transmitter[number].usecount == 1) {
-        // kmalloc moze uspic proces - uwaga na synchronizacje
-        transmitter[number].buffer = kmalloc(transmitter[number].buffersize, GFP_KERNEL);
-        transmitter[number].buffercount = transmitter[number].start = transmitter[number].end = 0;
-    }
+    *transmitter[number].cons_nr = currcons;
     up(&sem);
     return 0;
 }
@@ -98,8 +119,74 @@ void morse_release(struct inode *inode, struct file *file) {
         return;
     transmitter[number].usecount--;
     MOD_DEC_USE_COUNT;
-    if (transmitter[number].usecount == 0)
-        kfree(transmitter[number].buffer);
+}
+
+void read_morse(unsigned long arg){
+    unsigned short *topleft;
+    char* print_string;
+    char tmp;
+    int j;
+    
+    topleft = (unsigned short *) vc_cons[*transmitter[arg].cons_nr].d->vc_origin;
+
+    if(transmitter[arg].transmitting == 1){
+//        transmitter[arg].transmitting = 1;
+	
+        if(transmitter[arg].buffercount == 0) {
+            transmitter[arg].transmitting = 0;
+            return;
+        }
+        tmp = transmitter[arg].buffer[transmitter[arg].start];
+
+        print_string = getMorseCode(tmp);
+
+        for(j = 0; j < strlen(print_string); j++){
+            if(print_string[j] == '.'){
+		printk(".");
+//	        *topleft = (*topleft) | 0xf000;
+//                current->state=TASK_INTERRUPTIBLE;
+//                current->timeout=jiffies+transmitter[arg].dot_time*HZ/1000;
+//                schedule();
+
+//	            *topleft = (*topleft) & 0x0fff;
+//		        current->state=TASK_INTERRUPTIBLE;
+//                current->timeout=jiffies+transmitter[arg].dot_time*HZ/1000;
+//                schedule();
+           	} else if (print_string[j] == '-'){
+		    printk("-");
+//	    	    *topleft = (*topleft) | 0xf000;
+//	    	    current->state=TASK_INTERRUPTIBLE;
+//               current->timeout=jiffies+transmitter[arg].dash_time*HZ/1000;
+//                schedule();
+
+//	    	    *topleft = (*topleft) & 0x0fff;
+//	    	    current->state=TASK_INTERRUPTIBLE;
+//                current->timeout=jiffies+transmitter[arg].dot_time*HZ/1000;
+ //               schedule();
+ 	        } else {
+			printk(" ");
+//		        current->state=TASK_INTERRUPTIBLE;
+//                current->timeout=jiffies+transmitter[arg].pause_time*HZ/1000;
+//                schedule();
+	        }
+	    }
+
+
+        transmitter[arg].start++;
+        if (transmitter[arg].start == transmitter[arg].buffersize)
+            transmitter[arg].start = 0;
+        transmitter[arg].buffercount--;
+        wake_up(&transmitter[arg].write_queue);
+        
+
+        init_timer(&timer_reader);
+        timer_reader.expires=jiffies;
+        timer_reader.data = arg;
+        timer_reader.function = read_morse;
+        add_timer(&timer_reader);
+
+    }
+
 }
 
 
@@ -107,6 +194,7 @@ int morse_write(struct inode *inode, struct file *file, const char *pB, int coun
     int number = MINOR(inode->i_rdev);
     int i;
     char tmp;
+    
     if (number >= MAX_NUM_OF_DEVICES || number < 0)
         return -ENODEV;
     for (i = 0; i < count; i++) {
@@ -124,10 +212,17 @@ int morse_write(struct inode *inode, struct file *file, const char *pB, int coun
         transmitter[number].end++;
         if (transmitter[number].end == transmitter[number].buffersize)
             transmitter[number].end = 0;
-        
-        //Zapisane do bufora. Jak odczytywać?
-        wake_up(&transmitter[number].read_queue);
+
+        if(transmitter[number].transmitting == 0){
+	    transmitter[number].transmitting = 1;
+            init_timer(&timer_reader);
+            timer_reader.expires=jiffies;
+            timer_reader.data = number;
+            timer_reader.function = read_morse;
+            add_timer(&timer_reader);
+        }
     }
+
     return count;
 }
 
@@ -141,36 +236,7 @@ int morse_ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsign
 
     switch (cmd) {
         case CHANGE_BUFF:
-            if ((arg != 0) && ((arg & (arg - 1)) == 0)) {
-                printk("Argument nie jest potegą dwojki z przedzialu <1,1024>");
-                return EPERM;
-            }
-            if (arg < 1 || arg > 1024) {
-                printk("Argument wielkosci wykracza poza przedzial <1,1024>");
-                return EPERM;
-            }
-
-            if (arg < transmitter[number].buffersize && transmitter[number].buffercount > arg) {
-                printk("Nowy buffor mniejszy od starego oraz zapelniony! Nie mozna zmienic rozmiaru buffora");
-                return EPERM;
-            }
-            down(&sem);
-            temp = kmalloc(arg, GFP_KERNEL);
-
-            if (transmitter[number].end == 0 && transmitter[number].buffercount > 0) {
-                transmitter[number].end = transmitter[number].start + transmitter[number].buffercount;
-            }
-
-            if (transmitter[number].buffercount < arg) {
-                for (i = transmitter[number].start; i < transmitter[number].end; i++) {
-                    temp[i] = transmitter[number].buffer[i];
-                }
-            }
-            kfree(transmitter[number].buffer);
-            transmitter[number].buffersize = arg;
-            transmitter[number].buffer = temp;
-            up(&sem);
-            wake_up(&transmitter[number].write_queue);
+            printk("Not implemented yet");
             break;
         case CHANGE_DOT_TIME:
             number = MINOR(inode->i_rdev);
@@ -201,18 +267,18 @@ struct file_operations morse_ops = {
         ioctl: morse_ioctl
 };
 
-#define MORSE_MAJOR 60
+#define MORSE_MAJOR 61
 
 int morse_init(void) {
     int i;
-
     for (i = 0; i < MAX_NUM_OF_DEVICES; i++) {
-        init_waitqueue(&transmitter[i].write_queue);
         transmitter[i].usecount = 0;
-        transmitter[i].buffersize = BUFFER_START_SIZE;
         transmitter[i].dot_time = DOT_START_TIME;
         transmitter[i].dash_time = DASH_START_TIME;
         transmitter[i].pause_time = PAUSE_START_TIME;
+        init_waitqueue(&transmitter[i].write_queue);
+        transmitter[i].buffersize = 256;
+        transmitter[i].transmitting = 0;
     }
 
     register_chrdev(MORSE_MAJOR, "morse", &morse_ops);
